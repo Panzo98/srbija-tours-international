fa0c3cf9eb01580deb24051a19f9762a8e6d04d1
panzo <administrator@192.168.1.2>
2024-06-18 01:31:44 +0200

    ProfilScreen edit

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -8,23 +8,25 @@ import {
   Alert,
   Linking,
   Dimensions,
+  TextInput,
+  Image,
+  ScrollView,
 } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import * as SecureStore from "expo-secure-store";
 import CustomScreenHeader from "../components/CustomScreenHeader";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
+const { width, height } = Dimensions.get("window");
 
 const ProfileScreen = ({ navigation }) => {
-  const { width, height } = Dimensions.get("window");
-
   const user = useSelector((state) => state.auth.user);
-
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
     Alert.alert(
       "Potvrda",
-      "Da li zelite da se odjavite?",
+      "Da li želite da se odjavite?",
       [
         {
           text: "Ne",
@@ -82,49 +84,83 @@ const ProfileScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ flex: 1 }}>
-        <View style={styles.contentContainer}>
-          <Text style={styles.text}>
-            {user.first_name} {user.last_name}
-          </Text>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoLabel}>Email:</Text>
-            <Text style={styles.infoText}>{user.email}</Text>
-          </View>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoLabel}>Datum rođenja:</Text>
-            <Text style={styles.infoText}>{user.birth_date}</Text>
-          </View>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoLabel}>Telefon:</Text>
-            <Text style={styles.infoText}>{user.phone}</Text>
-          </View>
-        </View>
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          style={styles.list}
-          ListFooterComponent={
-            <Text style={styles.versionText}>v. 1.0.0 (1)</Text>
-          }
+      <View style={styles.header}>
+        <Image
+          source={require("../assets/images/company-logo-white.png")}
+          style={styles.logo}
         />
-        <TouchableOpacity
-          style={[
-            styles.logoutButton,
-            {
-              marginHorizontal: width * 0.04,
-              borderRadius: 5,
-              height: height * 0.07,
-              alignItems: "center",
-              justifyContent: "center",
-            },
-          ]}
-          onPress={handleLogout}
-        >
+      </View>
+      <ScrollView>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Text style={styles.title}>Korisnički nalog</Text>
+        </View>
+        <View style={styles.profileContainer}>
+          <View style={styles.profileInfoContainer}>
+            <View style={styles.avatarContainer}>
+              <Feather name="user" size={80} color="#188DFD" />
+            </View>
+            <Text
+              style={styles.username}
+            >{`${user.first_name} ${user.last_name}`}</Text>
+            <TouchableOpacity style={styles.editIcon}>
+              <Feather name="edit" size={24} color="#188DFD" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.infoSection}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Ime i prezime"
+                value={`${user.first_name} ${user.last_name}`}
+                editable={false}
+                placeholderTextColor="#888"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="E-mail adresa"
+                value={user.email}
+                editable={false}
+                placeholderTextColor="#888"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Broj telefona"
+                value={user.phone}
+                editable={false}
+                placeholderTextColor="#888"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Lozinka"
+                value="********"
+                editable={false}
+                placeholderTextColor="#888"
+              />
+            </View>
+          </View>
+          <View style={styles.sectionTitleContainer}>
+            <Text style={styles.sectionTitle}>Istorija rezervacija</Text>
+          </View>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            style={styles.list}
+            ListFooterComponent={
+              <Text style={styles.versionText}>v. 1.0.0 (1)</Text>
+            }
+          />
+        </View>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
           <Text style={styles.logoutButtonText}>ODJAVI SE</Text>
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -133,38 +169,76 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-  },
-  contentContainer: {
-    alignItems: "center",
     justifyContent: "center",
-    padding: 20,
+  },
+  header: {
     backgroundColor: "#188DFD",
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+    paddingVertical: 20,
+    alignItems: "center",
   },
-  text: {
+  logo: {
+    width: width * 0.6,
+    height: height * 0.1,
+    resizeMode: "contain",
+  },
+  title: {
+    color: "black",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  profileContainer: {
+    flex: 1,
+    paddingHorizontal: width * 0.04,
+  },
+  profileInfoContainer: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  avatarContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    padding: 20,
+    marginBottom: 10,
+  },
+  username: {
     fontSize: 22,
-    color: "#fff",
     fontWeight: "bold",
   },
-  infoContainer: {
+  editIcon: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+  },
+  infoSection: {
+    marginBottom: 20,
+  },
+  inputContainer: {
     flexDirection: "row",
-    marginTop: 5,
-    marginBottom: 5,
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    height: 50,
+    borderColor: "#ccc",
+    borderWidth: 1,
   },
-  infoLabel: {
+  input: {
+    flex: 1,
     fontSize: 16,
-    color: "#fff",
+    color: "#000",
+  },
+  sectionTitleContainer: {
+    marginVertical: 10,
+  },
+  sectionTitle: {
+    fontSize: 18,
     fontWeight: "bold",
-    marginRight: 5,
-  },
-  infoText: {
-    fontSize: 16,
-    color: "#fff",
   },
   list: {
     flex: 1,
-    backgroundColor: "#fff",
   },
   item: {
     padding: 15,
@@ -183,6 +257,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#188DFD",
     padding: 15,
     alignItems: "center",
+    marginHorizontal: width * 0.04,
+    borderRadius: 50,
   },
   logoutButtonText: {
     color: "white",
