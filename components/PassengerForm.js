ec3f3e83f27b96998d3281904dc18fa21da169a0
panzo <administrator@192.168.1.2>
2024-06-18 12:51:49 +0200

    Prepravka na unosu putnikaa

--- components/PassengerForm.js
+++ components/PassengerForm.js
@@ -16,40 +16,6 @@ import { formatPrice } from "../utils/formatPrice";
 
 const { width, height } = Dimensions.get("window");
 
-const getMinMaxDateForCategory = (category) => {
-  const currentYear = new Date().getFullYear();
-  let minDate, maxDate;
-
-  switch (category) {
-    case 1:
-      minDate = new Date(currentYear - 60, 0, 1);
-      maxDate = new Date(currentYear - 26, 11, 31);
-      break;
-    case 2:
-      minDate = new Date(currentYear - 2, 0, 1);
-      maxDate = new Date(currentYear, 11, 31);
-      break;
-    case 3:
-      minDate = new Date(currentYear - 12, 0, 1);
-      maxDate = new Date(currentYear - 3, 11, 31);
-      break;
-    case 4:
-      minDate = new Date(currentYear - 26, 0, 1);
-      maxDate = new Date(currentYear - 13, 11, 31);
-      break;
-    case 5:
-      minDate = new Date(1900, 0, 1);
-      maxDate = new Date(currentYear - 60, 11, 31);
-      break;
-    default:
-      minDate = new Date(1900, 0, 1);
-      maxDate = new Date(currentYear, 11, 31);
-      break;
-  }
-
-  return { minDate, maxDate };
-};
-
 const PassengerForm = ({ index, passenger }) => {
   const passengersFullInfo = useSelector(
     (state) => state.searchReducer.passengersFullInfo
@@ -66,8 +32,6 @@ const PassengerForm = ({ index, passenger }) => {
   };
 
   const [user, setUser] = useState(initialUser);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [tempBirthday, setTempBirthday] = useState(new Date());
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -88,96 +52,56 @@ const PassengerForm = ({ index, passenger }) => {
     });
   };
 
-  const onChangeDate = (event, selectedDate) => {
-    if (selectedDate) {
-      setTempBirthday(selectedDate);
-    }
-  };
-
-  const confirmDate = () => {
-    handleChange("birthday", format(tempBirthday, "yyyy-MM-dd"));
-    setShowDatePicker(false);
-  };
-
-  const formatCategoryToDisplay = (category) => {
-    switch (category) {
-      case 1:
-        return "Odrasli 26-60";
-      case 2:
-        return "Bebe 0-2";
-      case 3:
-        return "Deca 3-12";
-      case 4:
-        return "Mladi 13-26";
-      case 5:
-        return "Senior 60+";
-      default:
-        return "Pogrešna kategorija";
-    }
-  };
-
-  const { minDate, maxDate } = getMinMaxDateForCategory(passenger.category);
-
   return (
     <View style={styles.container}>
-      <Text style={styles.infoText}>Unesite podatke putnika za kategoriju</Text>
-      <Text style={styles.categoryText}>
-        "{formatCategoryToDisplay(passenger.category)}"
-      </Text>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text
+          style={{
+            height: 25,
+            width: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            verticalAlign: "center",
+            backgroundColor: "#188dfd",
+            marginRight: 10,
+            color: "white",
+            fontSize: 16,
+            fontWeight: "bold",
+          }}
+        >
+          {index + 1}
+        </Text>
+        <Text>Unesite podatke</Text>
+      </View>
       <TextInput
         style={styles.input}
-        placeholder="Ime: Pera"
+        placeholder="Ime (obavezno)"
+        placeholderTextColor={"#868484"}
         value={user.name}
         onChangeText={(text) => handleChange("name", text)}
       />
       <TextInput
         style={styles.input}
-        placeholder="Prezime: Perić"
+        placeholder="Prezime (obavezno)"
+        placeholderTextColor={"#868484"}
         value={user.lastName}
         onChangeText={(text) => handleChange("lastName", text)}
       />
       <TextInput
         style={styles.input}
-        placeholder="Broj telefona: +381 62 432 34 23"
+        placeholder="Broj telefona (obavezno)"
+        placeholderTextColor={"#868484"}
         value={user.phone}
         onChangeText={(text) => handleChange("phone", text)}
       />
-      <TouchableOpacity
-        onPress={() => setShowDatePicker(true)}
-        style={styles.dateInput}
-      >
-        <Text style={user.birthday ? styles.dateText : styles.placeholderText}>
-          {user.birthday ? user.birthday : "Datum rođenja: 1990-05-21"}
-        </Text>
-      </TouchableOpacity>
-      {showDatePicker && (
-        <Modal
-          transparent={true}
-          animationType="slide"
-          visible={showDatePicker}
-          onRequestClose={() => setShowDatePicker(false)}
-        >
-          <View style={styles.datePickerContainer}>
-            <View style={styles.datePicker}>
-              <DateTimePicker
-                value={tempBirthday}
-                style={{ backgroundColor: "white" }}
-                mode="date"
-                display={Platform.OS === "ios" ? "spinner" : "default"}
-                onChange={onChangeDate}
-                minimumDate={minDate}
-                maximumDate={maxDate}
-              />
-              <TouchableOpacity
-                style={styles.confirmButton}
-                onPress={confirmDate}
-              >
-                <Text style={styles.confirmButtonText}>Potvrdi</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </Modal>
-      )}
+      <TextInput
+        style={styles.input}
+        placeholder="Datum rodjenja (obavezno)"
+        placeholderTextColor={"#868484"}
+        value={user.birthday}
+        onChangeText={(text) => handleChange("birthday", text)}
+      />
       <Text style={styles.priceInfoText}>
         Karta za izabranu kategoriju iznosi
       </Text>
@@ -214,6 +138,7 @@ const styles = StyleSheet.create({
     padding: height * 0.015,
     marginBottom: height * 0.02,
     fontSize: height * 0.022,
+    fontWeight: "bold",
   },
   dateInput: {
     borderWidth: 1,
